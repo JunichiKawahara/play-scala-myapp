a13bb5d0b5c85b36bfcbdc2a43a7be4f684f02f2
Kawahara Junichi <kawahara-j@IMAC5-6.local>
2020-02-26 13:26:13 +0900

    Chapter 4-2 JDBCによるデータベースアクセス

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,31 +1,34 @@
 package controllers
 
+import java.sql._
 import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import play.api.db._
 
 @Singleton
-class HomeController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
-// class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(db: Database, cc: MessagesControllerComponents)
+    extends MessagesAbstractController(cc) {
 
-  import MyForm._
-
-  def index() = Action { implicit request =>
-    Ok(views.html.index(
-      "これはコントローラーで用意したメッセージです。",
-      myform
-    ))
-  }
-
-  def form() = Action { implicit request =>
-    val form = myform.bindFromRequest
-    val data = form.get
-    Ok(views.html.index(
-      "name: " + data.name + ", password: " + data.pass +
-        ", radio: " + data.radio,
-      form
-    ))
-  }
+    def index() = Action { implicit request =>
+        var msg = "database record: <br><ul>"
+        try {
+            db.withConnection { conn =>
+                val stmt = conn.createStatement
+                val rs = stmt.executeQuery("SELECT * FROM people")
+                while (rs.next) {
+                    msg += "<li>" + rs.getInt("id") + ":" + rs.getString("name") + "</li>"
+                }
+                msg += "</ul>"
+            }
+        } catch {
+            case e: SQLException =>
+                msg = "<li>no record...</li>"
+        }
+        Ok(views.html.index(
+            msg
+        ))
+    }
 }
